1bb6af53c1efdd161505fcabded6fc64babbf107
Devulapalli <vdevulapalli@deloitte.com>
2019-03-20 01:46:10 +0530

    added product service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http'
 import  'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
-  private _albumsUrl: string = '../assets/album.json';
+  private _albumUrl: string = '../assets/album.json';
   constructor(private _http: Http) {
 
   }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumsUrl).map(function (response) {
+  getAlbum(id: number):Observable<any> {
+    return this._http.get(this._albumUrl).map(function (response) {
       return response.json();
     });
   }
